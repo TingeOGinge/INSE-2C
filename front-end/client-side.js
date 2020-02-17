1490c66ba30e6e3274034fbee6f9e62f44260c32
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2020-02-17 14:58:50 +0000

    add client-side api file with function exports
    Co-Authored-By: null <JamesDBardsley@users.noreply.github.com>

---
+++ front-end/client-side.js
@@ -0,0 +1,47 @@
+/*  registerUser and login input stucture is as follows:
+    {
+      username,
+      password,
+    }
+
+    scheduleRecipe input structure is as follows:
+    {
+
+    }
+*   Responses will be thrown if there is an error
+*/
+
+async function registerUser(data) {
+  const url = 'http://localhost:5000/api/registerUser';
+  const requestOptions = generatePostRequestOptions(data);
+  const response = await fetch(url, requestOptions);
+
+  if (!response.ok) throw response;
+}
+
+async function login(data) {
+  const url = 'http://localhost:5000/api/login';
+  const requestOptions = generatePostRequestOptions(data);
+  const response = await fetch(url, requestOptions);
+
+  if (!response.ok) throw response;
+  return await response.json();
+}
+
+async function scheduleRecipe(data) {
+  const url = 'http://localhost:5000/api/scheduleRecipe';
+  const requestOptions = generatePostRequestOptions(data);
+  const response = await fetch(url, requestOptions);
+
+  if (!response.ok) throw response;
+}
+
+function generatePostRequestOptions(data) {
+  return {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  };
+}
+
+export {registerUser, login, scheduleRecipe};
