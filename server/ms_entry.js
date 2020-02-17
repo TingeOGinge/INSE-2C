b1e8b8d0ca0b198a1a4d1a0c18dfe8a4f46b2e45
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2020-02-17 19:54:31 +0000

    :art: minor refactor

--- server/ms_entry.js
+++ server/ms_entry.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
-const {extractToken, validateLogin, validateSession, hashPassword} = require('./ms_auth.js');
+const {validateLogin, validateSession, hashPassword} = require('./ms_auth.js');
 const {users, registerUser, scheduleRecipe, retrieveUserSchedule} = require('./ms_account.js');
 
 
@@ -11,19 +11,10 @@ app.use(express.static(path.join(__dirname, '..', 'front-end')));
 app.use(cors());
 
 app.get('/api/getUsers', (req, res) => { res.json(users); });
-app.get('/api/protectedRoute', extractToken, validateSession, showProtectedResources);
-app.get('/api/getUserSchedule', extractToken, validateSession, retrieveUserSchedule);
+app.get('/api/getUserSchedule', validateSession, retrieveUserSchedule);
 
 app.post('/api/registerUser', hashPassword, registerUser);
 app.post('/api/login', validateLogin);
-app.post('/api/scheduleRecipe', extractToken, validateSession, scheduleRecipe);
+app.post('/api/scheduleRecipe', validateSession, scheduleRecipe);
 
 app.listen(5000, () => console.log('Server started on port 5000'));
-
-
-// Function created to test authorization process
-function showProtectedResources(req, res) {
-  res.json({
-    message: "Protected resources successfully accessed"
-  });
-}
