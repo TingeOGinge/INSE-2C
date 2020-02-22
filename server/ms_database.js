93aae17ce42cad983d273113eccf3e13e8199d36
JamesDBardsley <jamesdbardsley@gmail.com>
2020-02-22 16:38:44 +0000

    add query function to ms_database

--- server/ms_database.js
+++ server/ms_database.js
@@ -1,14 +1,45 @@
-const {Client} = require('pg');
-const connectionString = 'postgresql://james:inse2c@localhost:5432/ecochefdb'
+const {Pool} = require('pg');
+
+const connectionString = 'postgresql://james:inse2c@localhost:5432/ecochefdb';
+
 //connect
-const client = new Client({
-   connectionString
+const pool = new Pool({
+  connectionString
 });
 
-client.connect()
-.then(() => console.log("Connection Established to Database"))
-.catch(e => console.log(e))
-.finally(() => client.end());
+const queryDictionary = {
+  searchAccountName: 'SELECT * FROM account WHERE account_username = $1',
+  createAccount: 'INSERT INTO account (account_username, account_password) values($1, $2)',
+  removeAccount: 'DELETE FROM account WHERE account_id = $1 AND account_username = $2',
+  searchRecipeName: 'SELECT * FROM recipe WHERE recipe_name like $1',
+  searchRecipeIngredients: 'select * ' +
+                            'from recipe a ' +
+                            'left join recipe_ingredient b on a.recipe_id = b.recipe_id ' +
+                            'left join ingredient c on b.ingredient_id = c.ingredient_id ' +
+                            'where ingredient_name similar to $1',
+  scheduleRecipe: 'INSERT INTO account_recipe (account_id, recipe_id, scheduled_time) values($1, $2, $3)'
+}
+
+async function query(queryFlag, parameters) {
+  if (!Array.isArray(parameters)) parameters = [parameters];
+  if (queryFlag === 'searchByIngredients') parameters = `%(${parameters.join('|')})%`;
+
+  const client = await pool.connect();
+  try {
+    const queryString = queryDictionary[queryFlag]
+    if (queryString) {
+      const res = await client.query(queryString, parameters);
+      return res;
+    } else {
+        throw new Error('queryFlag not recognised');
+    }
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    client.release();
+  }
+}
+
 
  /* const { Client } = require('pg');
 
@@ -29,4 +60,4 @@ client.query('SELECT NOW()', (err, res) => {
     client.end();
   })
 
-  */
+  */module.exports = {query};
