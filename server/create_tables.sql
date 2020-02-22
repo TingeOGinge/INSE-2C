79082c42ef5039b9320a4d6cd8329872f989b36b
JamesDBardsley <jamesdbardsley@gmail.com>
2020-02-22 15:14:57 +0000

    update the database to include serial values for id and array text for recipe method

--- server/create_tables.sql
+++ server/create_tables.sql
@@ -1,35 +1,35 @@
 
 
 create table ingredient (
-ingredient_id varchar(8) primary key,
+ingredient_id serial primary key,
 ingredient_name varchar(32) not null
 );
 
 create table recipe (
-recipe_id varchar(8) primary key,
+recipe_id serial primary key,
 recipe_name varchar(32) not null,
 recipe_cooking_time integer not null,
-recipe_method text not null,
+recipe_method text [] not null,
 recipe_serving_size integer not null,
 recipe_calories integer not null
 );
 
 create table recipe_ingredient (
 primary key (recipe_id, ingredient_id),
-recipe_id varchar(8) references recipe(recipe_id),
-ingredient_id varchar(8) references ingredient(ingredient_id)
+recipe_id integer references recipe(recipe_id),
+ingredient_id integer references ingredient(ingredient_id)
 );
 
 create table account (
-account_id varchar(8) primary key,
+account_id serial primary key,
 account_username varchar(32) not null,
 account_password varchar(100) not null
 );
 
 create table account_recipe (
 primary key (account_id, recipe_id),
-account_id varchar(8) references account(account_id),
-recipe_id varchar(8) references recipe(recipe_id),
+account_id integer references account(account_id),
+recipe_id integer references recipe(recipe_id),
 scheduled_time timestamp not null 
 );
 
