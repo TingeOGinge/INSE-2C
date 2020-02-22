54e757be610ebad115286a978d63abe0dc784a25
JamesDBardsley <jamesdbardsley@gmail.com>
2020-02-22 17:49:54 +0000

    add create database to create_tables.sql

--- server/create_tables.sql
+++ server/create_tables.sql
@@ -1,4 +1,5 @@
-
+create database ecochefdb;
+\c ecochefdb
 
 create table ingredient (
 ingredient_id serial primary key,
@@ -23,13 +24,13 @@ ingredient_id integer references ingredient(ingredient_id)
 create table account (
 account_id serial primary key,
 account_username varchar(32) not null,
-account_password varchar(100) not null
+account_password varchar(100) not null,
+constraint account_username unique
 );
 
 create table account_recipe (
 primary key (account_id, recipe_id),
 account_id integer references account(account_id),
 recipe_id integer references recipe(recipe_id),
-scheduled_time timestamp not null 
+scheduled_time timestamp not null
 );
-
