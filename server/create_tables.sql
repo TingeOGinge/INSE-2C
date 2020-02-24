c5a19b9c835b119a0fc99b229670df2fad8d918a
JamesDBardsley <jamesdbardsley@gmail.com>
2020-02-24 15:46:17 +0000

    update primary key on account_recipe to include schedule_time.

--- server/create_tables.sql
+++ server/create_tables.sql
@@ -29,7 +29,7 @@ constraint account_username unique
 );
 
 create table account_recipe (
-primary key (account_id, recipe_id),
+primary key (account_id, recipe_id, scheduled_time),
 account_id integer references account(account_id),
 recipe_id integer references recipe(recipe_id),
 scheduled_time timestamp not null
