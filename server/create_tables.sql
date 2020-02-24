2c9c3bf7893f9368a99804fb347a8920c16fe227
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2020-02-24 19:40:59 +0000

    fix bugs in db creation and population

--- server/create_tables.sql
+++ server/create_tables.sql
@@ -21,20 +21,20 @@ recipe_calories integer not null
 
 create table recipe_ingredient (
 primary key (recipe_id, ingredient_id),
-recipe_id integer references recipe(recipe_id),
-ingredient_id integer references ingredient(ingredient_id)
+recipe_id integer references recipe(recipe_id) ON DELETE CASCADE,
+ingredient_id integer references ingredient(ingredient_id) ON DELETE CASCADE
 );
 
 create table account (
 account_id serial primary key,
 account_username varchar(32) not null,
 account_password varchar(100) not null,
-constraint account_username unique
+UNIQUE(account_username)
 );
 
 create table account_recipe (
 primary key (account_id, recipe_id, scheduled_time),
-account_id integer references account(account_id),
-recipe_id integer references recipe(recipe_id),
+account_id integer references account(account_id) ON DELETE CASCADE,
+recipe_id integer references recipe(recipe_id) ON DELETE CASCADE,
 scheduled_time timestamp not null
 );
