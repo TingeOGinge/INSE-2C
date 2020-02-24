4b806467e83ee701eca265a191be487a0dc764cb
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2020-02-24 15:19:03 +0000

    integrate all existint account functions with database

--- server/ms_account.js
+++ server/ms_account.js
@@ -2,7 +2,8 @@ const {query}  = require('./ms_database.js');
 
 async function registerUser(req, res) {
   try {
-    const queryResponse = await query('createAccount', [req.body.username, req.body.password]);
+    const params = [req.body.username, req.body.password];
+    const queryResponse = await query('createAccount', params);
     (queryResponse) ? res.sendStatus(201) : res.sendStatus(409);
   } catch (err) {
     console.log(err.stack);
@@ -10,27 +11,24 @@ async function registerUser(req, res) {
   }
 }
 
-function scheduleRecipe(req, res) {
-  const user = users.find(user => user.username === req.body.tokenData.username);
-  if (checkScheduleConflict(user.scheduledRecipes, req.body.scheduledTime)) {
-    res.sendStatus(409);
-  } else {
-    user.scheduledRecipes.push({
-      recipeID: req.body.recipeID,
-      scheduledTime: req.body.scheduledTime
-    });
-    res.sendStatus(200);
+async function scheduleRecipe(req, res) {
+  try {
+    const params = [
+      req.tokenPayload.account_id,
+      req.body.recipe_id,
+      req.body.scheduled_time
+    ];
+    const queryResponse = await query('scheduleRecipe', params);
+    (queryResponse) ? res.sendStatus(200) : res.sendStatus(409);
+  } catch (err) {
+    console.log(err.stack);
+    res.sendStatus(500);
   }
-}
 
-function checkScheduleConflict(schedule, proposedTime) {
-  const conflict = schedule.find(recipe => recipe.scheduledTime === proposedTime);
-  return !(conflict === undefined);
 }
 
 async function retrieveUserSchedule(req, res) {
   try {
-    console.log(req.tokenPayload);
     const queryResponse = await query('getUserSchedule', req.tokenPayload.account_id);
     (queryResponse.rows.length > 0) ? res.json(queryResponse.rows) : res.sendStatus(404);
   } catch (err) {
