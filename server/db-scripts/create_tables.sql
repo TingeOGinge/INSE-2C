01a287db6332733be43e1980cd3ef32815f9d36c
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2020-03-04 15:26:43 +0000

    move db scripts into separate folder

--- server/db-scripts/create_tables.sql
+++ server/db-scripts/create_tables.sql
@@ -7,14 +7,15 @@ grant usage, select on all sequences in schema public to myuser;
 
 create table ingredient (
 ingredient_id serial primary key,
-ingredient_name varchar(32) not null
+ingredient_name varchar(256) UNIQUE not null
 );
 
 create table recipe (
 recipe_id serial primary key,
-recipe_name varchar(32) not null,
-recipe_cooking_time integer not null,
+recipe_name varchar(256) UNIQUE not null,
+recipe_cooking_time interval not null,
 recipe_method text [] not null,
+recipe_ingredients text [] not null,
 recipe_serving_size integer not null,
 recipe_calories integer not null
 );
@@ -38,4 +39,15 @@ primary key (account_id, recipe_id, scheduled_time),
 account_id integer references account(account_id) ON DELETE CASCADE,
 recipe_id integer references recipe(recipe_id) ON DELETE CASCADE,
 scheduled_time timestamp not null
+);
+
+create table dietary_restrictions (
+  diet_restriction_id serial primary key,
+  diet_restriction_name varchar(50) not null
+);
+
+create table recipe_dietary (
+  primary key (recipe_id, diet_restriction_id),
+  recipe_id integer references recipe(recipe_id) ON DELETE CASCADE,
+  diet_restriction_id integer references dietary_restrictions(diet_restriction_id) ON DELETE CASCADE
 );
