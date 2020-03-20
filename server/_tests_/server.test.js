4cb3dad19c6b9d268e868fc05b5ccfc0c358c597
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2020-03-20 13:47:43 +0000

    implement crypto to generate randon usernames for testing

--- server/_tests_/server.test.js
+++ server/_tests_/server.test.js
@@ -1,9 +1,11 @@
 const request = require('supertest');
 const path = require('path');
 const app = require(path.join(__dirname, '..', 'ms_entry.js'));
+const crypto = require('crypto');
 
 describe("Test the root path", () => {
   test("It should respond to the GET with 200", async () => {
+const randomUsername = crypto.randomBytes(10).toString('hex');
     const response = await request(app).get("/");
     expect(response.statusCode).toBe(200);
   });
