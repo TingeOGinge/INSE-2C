2bca59ace06965649a77a459e2ddea22854d3cc6
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2020-03-20 13:48:07 +0000

    add test for adding user with random username

--- server/_tests_/server.test.js
+++ server/_tests_/server.test.js
@@ -2,11 +2,26 @@ const request = require('supertest');
 const path = require('path');
 const app = require(path.join(__dirname, '..', 'ms_entry.js'));
 const crypto = require('crypto');
+const {poolEnd} = require(path.join(__dirname, '..', 'ms_database.js'));
 
-describe("Test the root path", () => {
-  test("It should respond to the GET with 200", async () => {
 const randomUsername = crypto.randomBytes(10).toString('hex');
+
+describe("Test the server routes", () => {
+
+  afterAll(async () => {
+    await poolEnd();
+  });
+
+  test("GET '/' should respond with 200", async () => {
     const response = await request(app).get("/");
     expect(response.statusCode).toBe(200);
+  });
+
+  test("POST '/api/registerUser' with valid credentials", async () => {
+    const response = await request(app).post('/api/registerUser')
+                            .set('Content-Type', 'application/json')
+                            .send(`{"username": "${randomUsername}}", "password": "password"}`);
+    console.log(response.text);
+    expect(response.statusCode).toBe(201);
   });
 });
