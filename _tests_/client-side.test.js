5da8edf34724931a30c08ded62c199a2d1c413b2
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2020-03-21 14:22:15 +0000

    chain mock-fetch responses when testing client-side api
    - refactored in both login and register tests

--- _tests_/client-side.test.js
+++ _tests_/client-side.test.js
@@ -1,8 +1,5 @@
-const crypto = require('crypto');
 const api = require('../front-end/client-side.js');
 
-const randomUsername = crypto.randomBytes(10).toString('hex');
-const scheduledTime = new Date().toISOString();
 const exampleToken = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImFjY291bnRfdXNlcm5hbWUiOiJEaWxsb24iLCJhY2NvdW50X2lkIjoxNDUsImFkbWluX3N0YXR1cyI6dHJ1ZX0sImlhdCI6MTU4NDc3NjYxMSwiZXhwIjoxNTg0ODYzMDExfQ.jWPk1jinxvLuQuK_oFE6dh_nnPfRTUaGeOLMZTXtWuNfJSzdcSdeGS-q4TVoXEMhslZ7x-h9L5J2pgsjdNemKDsDafbrvTCdII1BZRZOfTrmRht__bwQm9CJ5DbnStwBu52AfUCrvaGnJCQR3UAcwkvbdSgiRwf4l5bAB_cT9sDmPR9FJFazeZc8QRCWANGLW5FrUZ7uFC95AX627DuxxSNOTStO298OjyHm80cFleN8zKYxms3tSpypG5qYgL5YMLEY8OAjtWR6eM2avlacatPoJKeoUaJzQ-ZP3wmwres383pEMJ72_AJHPHEEAiwQhEWOWYxodur0dUMaEA4gJA';
 
 describe("Test the client-side API functions", () =>{
@@ -11,17 +8,67 @@ describe("Test the client-side API functions", () =>{
     fetch.resetMocks();
   });
 
-  test("Test registering a valid new user", async () => {
-    fetch.mockResponseOnce('', {status: 201});
-    const response = await api.registerUser(`{"username": "${randomUsername}", "password": "password"}`);
+  test("Test registering a new user", async () => {
+    fetch.mockResponses(
+      [
+        '',
+        { status: 201 }
+      ],
+      [
+        '',
+        { status: 409 }
+      ],
+      [
+        '',
+        { status: 500 }
+      ]
+    );
+
+    // Valid regsiter credentials
+    const response = await api.registerUser(`[{"username":"username","password":"password"}]`);
     expect(response.status).toBe(201);
     expect(fetch.mock.results[0].type).toEqual('return');
+
+    // Username conflict
+    await expect(api.registerUser(`[{"username":"username","password":"password"}]`))
+            .rejects.toThrow('Conflict');
+
+    // Internal server error
+    await expect(api.registerUser()).rejects.toThrow('Internal Server Error');
   });
 
-  test("Test registering an invalid new user", async () => {
-    fetch.mockResponseOnce('', {status: 409});
-    await expect(api.registerUser(`{"username": "${randomUsername}", "password": "password"}`)).rejects.toThrow('Conflict');
-  });
+  test("Test logging in", async () => {
+    fetch.mockResponses(
+      [
+        `{"token":"${exampleToken}"}`,
+        { status: 200 }
+      ],
+      [
+        '',
+        { status: 404 }
+      ],
+      [
+        '',
+        { status: 403 }
+      ],
+      [
+        '',
+        { status: 500 }
+      ]
+    );
 
+    // Valid login
+    const token = await api.login();
+    expect(token).toEqual(exampleToken);
+
+    // User not found
+    await expect(api.login()).rejects.toThrow('Not Found');
+
+    // Invalid password
+    await expect(api.login()).rejects.toThrow('Forbidden');
+
+    // Internal server error
+    await expect(api.registerUser()).rejects.toThrow('Internal Server Error');
+  });
 
 });
