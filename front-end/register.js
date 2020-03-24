67d8cd73d33a3a850070102c1525ce20c5659395
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2020-03-24 14:04:44 +0000

    add register JS functionality

---
+++ front-end/register.js
@@ -0,0 +1,38 @@
+/* global registerUser */
+
+const el = {};
+
+function prepareHandles() {
+  el.username = document.querySelector('#username');
+  el.password = document.querySelector('#password');
+  el.registerButton = document.querySelector('#registerButton');
+}
+
+async function register() {
+  if (el.username.value !== "" && el.password.value !== "") {
+    try {
+      const registerObj = {username: el.username.value, password: el.password.value};
+      await registerUser(registerObj);
+      window.location.href = 'login.html';
+    } catch(err) {
+      // Either 409 Conflict or 500 Internal Server Error if unforseen issue
+      console.log(err);
+    }
+  }
+}
+
+function checkKeys(e) {
+  if (e.key === 'Enter') register();
+}
+
+function addEventListeners() {
+  el.registerButton.addEventListener('click', register);
+  el.password.addEventListener('keyup', checkKeys);
+}
+
+function init() {
+  prepareHandles();
+  addEventListeners();
+}
+
+window.addEventListener('load', init);
