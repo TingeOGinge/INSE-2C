e2f3e942d7823cf1da91d23b017954a715d20cd8
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2020-03-26 08:02:31 +0000

    :shirt:

--- front-end/javascript/profile.js
+++ front-end/javascript/profile.js
@@ -1,64 +1,62 @@
-/* global getUserSchedule */
+/* global getUserSchedule, deleteFromSchedule */
 
 const el = {};
 let recipes;
 
-function loadRecipes(){
+function loadRecipes() {
+  for (const recipe of recipes){
+    let recipeIDClass = `recipeID${recipe.recipe_id}`;
+    const recipeContainer = document.createElement("div");
+    recipeContainer.classList.add(
+      'recipe',
+      'recipeEventListener',
+      recipeIDClass
+    );
+    el.recipeTitle.append(recipeContainer);
 
-    for (const recipe of recipes){
-      let recipeIDClass = `recipeID${recipe.recipe_id}`;
-      const recipeContainer = document.createElement("div");
-      recipeContainer.classList.add(
-        'recipe',
+    const recipeHeader = document.createElement("h3");
+    recipeHeader.classList.add(
+      'recipeTitle',
+      'recipeEventListener',
+      recipeIDClass
+    );
+    recipeHeader.innerHTML = recipe.recipe_name;
+    recipeContainer.append(recipeHeader);
+
+    const ingredientTitle = document.createElement("h4");
+    ingredientTitle.classList.add(
+      'ingredientTitle',
+      'recipeEventListener',
+      recipeIDClass
+    );
+    ingredientTitle.textContent = "Ingredients:";
+    recipeContainer.append(ingredientTitle);
+
+    const recipeIngredients = document.createElement("ul");
+    recipeIngredients.id = 'recipeIngredient';
+    recipeIngredients.classList.add(
+      'recipeEventListener',
+      recipeIDClass
+    );
+    recipeContainer.append(recipeIngredients);
+
+    for (const ingredient of recipe.recipe_ingredients){
+      const recipeIngredient = document.createElement("li");
+      recipeIngredient.innerHTML = ingredient;
+      recipeIngredient.classList.add(
+        'ingredientList',
         'recipeEventListener',
         recipeIDClass
       );
-      el.recipeTitle.append(recipeContainer);
-
-      const recipeHeader = document.createElement("h3");
-      recipeHeader.classList.add(
-        'recipeTitle',
-        'recipeEventListener',
-        recipeIDClass
-      );
-      recipeHeader.innerHTML = recipe.recipe_name;
-      recipeContainer.append(recipeHeader);
-
-      const ingredientTitle = document.createElement("h4");
-      ingredientTitle.classList.add(
-        'ingredientTitle',
-        'recipeEventListener',
-        recipeIDClass
-      );
-      ingredientTitle.textContent = "Ingredients:";
-      recipeContainer.append(ingredientTitle);
-
-      const recipeIngredients = document.createElement("ul");
-      recipeIngredients.id = 'recipeIngredient';
-      recipeIngredients.classList.add(
-        'recipeEventListener',
-        recipeIDClass
-      );
-      recipeContainer.append(recipeIngredients);
-      
-      for (const ingredient of recipe.recipe_ingredients){
-        const recipeIngredient = document.createElement("li");
-        recipeIngredient.innerHTML = ingredient;
-        recipeIngredient.classList.add(
-          'ingredientList',
-          'recipeEventListener',
-          recipeIDClass
-        );
-        recipeContainer.append(recipeIngredient);
-      }
+      recipeContainer.append(recipeIngredient);
     }
-    document.querySelectorAll('.recipeEventListener').forEach(elem => {
-      elem.addEventListener('click', linkHandler);
-    });
   }
+  document.querySelectorAll('.recipeEventListener').forEach(elem => {
+    elem.addEventListener('click', linkHandler);
+  });
+}
 
-
-function linkHandler(e){
+function getRecipeFromEvent(e) {
   const elementClassList = e.target.classList;
   let recipeID;
   elementClassList.forEach(elementClass => {
@@ -66,8 +64,15 @@ function linkHandler(e){
       recipeID = elementClass.replace('recipeID', '');
     }
   });
+
   recipeID = parseInt(recipeID, 10);
   const chosenRecipe = recipes.find(recipe => recipe.recipe_id === recipeID);
+
+  return chosenRecipe;
+}
+
+function linkHandler(e){
+  const chosenRecipe = getRecipeFromEvent(e);
   window.localStorage.setItem('chosenRecipe', JSON.stringify(chosenRecipe));
   window.location.href = 'recipe.html';
 }
@@ -79,7 +84,22 @@ async function getSchedule() {
     const schedule = await getUserSchedule(token);
     return schedule;
   } catch(err) {
-    console.log(err.stack);
+    console.log(err);
+  }
+}
+
+async function deleteRecipe(e) {
+  const chosenRecipe = getRecipeFromEvent(e);
+  const token = window.localStorage.getItem('jwt');
+  try {
+    if(!token) throw new Error('No JWT found');
+    const data = {
+      recipe_id: chosenRecipe.recipe_id,
+      scheduled_time: chosenRecipe.scheduled_time
+    };
+    await deleteFromSchedule(data, token);
+  } catch(err) {
+    console.log(err);
   }
 }
 
