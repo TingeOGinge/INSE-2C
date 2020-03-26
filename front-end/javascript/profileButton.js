10c574cf70e09e76d61b9f4e908adfc239388721
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2020-03-26 17:40:47 +0000

    deactivate register and login button if user logged in

--- front-end/javascript/profileButton.js
+++ front-end/javascript/profileButton.js
@@ -1,11 +1,22 @@
 const elem = {
-  profileButton: document.querySelector('#btnProfile')
+  profileButton: document.querySelector('#btnProfile'),
+  loginButton: document.querySelector('#btnWhite'),
+  registerButton: document.querySelector('#btnBlue')
 };
 
 window.addEventListener('load', init);
 
 function init() {
-  const flag = window.localStorage.getItem('jwt') == null;
-  elem.profileButton.href = (flag) ? '#' : 'profile.html';
-  elem.profileButton.classList.toggle('class', flag);
+  if (window.localStorage.getItem('jwt') == null) {
+    elem.profileButton.href = '#';
+    elem.loginButton.href = 'login.html';
+    elem.registerButton = 'register.html';
+  } else {
+    elem.profileButton.href = 'profile.html';
+    elem.loginButton.href = '#';
+    elem.registerButton.href = '#';
+    console.log(elem.registerButton);
+  }
+
+
 }
