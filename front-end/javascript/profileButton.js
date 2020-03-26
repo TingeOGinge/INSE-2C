eb7920570fa27ed10001fb79cca27b11570c9301
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2020-03-26 17:48:05 +0000

    grey out deactivated buttons

--- front-end/javascript/profileButton.js
+++ front-end/javascript/profileButton.js
@@ -9,13 +9,22 @@ window.addEventListener('load', init);
 function init() {
   if (window.localStorage.getItem('jwt') == null) {
     elem.profileButton.href = '#';
+    elem.profileButton.classList.add('greyOutImage');
+
     elem.loginButton.href = 'login.html';
+    elem.loginButton.classList.remove('greyOutImage');
+
     elem.registerButton = 'register.html';
+    elem.registerButton.classList.remove('greyOutImage');
   } else {
     elem.profileButton.href = 'profile.html';
+    elem.profileButton.classList.remove('greyOutImage');
+
     elem.loginButton.href = '#';
+    elem.loginButton.classList.add('greyOutImage');
+
     elem.registerButton.href = '#';
-    console.log(elem.registerButton);
+    elem.registerButton.classList.add('greyOutImage');
   }
 
 
