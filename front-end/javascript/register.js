5e7cfa731baae6db69de1e1671492db5ec23b626
John Reid <john@JohnMacBookPro.local>
2020-03-27 14:43:35 +0000

    add documentation to register

--- front-end/javascript/register.js
+++ front-end/javascript/register.js
@@ -1,7 +1,16 @@
 /* global registerUser, module */
 
+/**
+ * @module User Register Page
+ */
+
 const el = {};
 
+/**
+ * prepareHandles - Selects elements from the document to be used later on by
+ * storing in the global element (el) class
+ *
+ */
 function prepareHandles() {
   el.username = document.querySelector('#username');
   el.password = document.querySelector('#password');
@@ -9,6 +18,17 @@ function prepareHandles() {
   el.registerPopup = document.querySelector('#registerPopup');
 }
 
+
+/**
+ * register - Allows user to register. Username and password from input boxes are
+ * stored in registerOBJ and passed to the global registerUser function and the
+ * user is redirected to the login page to use their newly created credentials.
+ * Should an error occur, the user is presented with an error message depending
+ * on the response from the server.
+ * Either 409 Conflict, 400 Bad Request or 500 Internal Server Error if unforseen issue
+ *
+ * @return {boolean}  catch error returns a false for testing purposes
+ */
 async function register() {
   if (el.username.value !== "" && el.password.value !== "") {
     try {
@@ -24,20 +44,30 @@ async function register() {
   }
 }
 
+/**
+ * checkKeys - allows return key to be used to add ingredients to list
+ * @param  {HTMLElement} e event
+ */
 function checkKeys(e) {
   if (e.key === 'Enter') register();
 }
 
+/**
+ * addEventListeners - Adds event listeners where needed
+ */
 function addEventListeners() {
   el.registerButton.addEventListener('click', register);
   el.password.addEventListener('keyup', checkKeys);
 }
 
-function init() {
+/**
+ * pageLoaded - Runs when the page is loaded - similar to a Java main function
+ */
+function pageLoaded() {
   prepareHandles();
   addEventListeners();
 }
 
-window.addEventListener('load', init);
+window.addEventListener('load', pageLoaded);
 
 if (typeof module === 'object') module.exports = {el, register};
