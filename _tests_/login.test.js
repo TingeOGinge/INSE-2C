5739e5a43bfcb719ef3ad0bfd9374acd29dd6531
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2020-03-27 12:40:35 +0000

    update tests for login

--- _tests_/login.test.js
+++ _tests_/login.test.js
@@ -21,29 +21,63 @@ describe("Tetst front-end/login.js", () => {
   const mockLoginUser = jest.fn(loginScript.loginUser);
 
   test("Test valid logging in of a user", async () => {
-    fetch.mockResponse(`{"token":"exampleJWT"}`, { status: 200 });
+    try {
+      fetch.mockResponse(`{"token":"exampleJWT"}`, { status: 200 });
 
-    loginScript.el.username.value = 'username';
-    loginScript.el.password.value = 'password';
-    expect(await mockLoginUser()).toBe(undefined);
-    expect(window.location.href).toEqual('index.html');
+      loginScript.el.username.value = 'username';
+      loginScript.el.password.value = 'password';
+      expect(await mockLoginUser()).toBe(undefined);
+      expect(window.location.href).toEqual('index.html');
+    } catch(err) {
+      console.log(err);
+    }
+
   });
 
   test("Test invalid logging in of a user (no user found)", async () => {
-    fetch.mockResponse('', { status: 404 });
+    try {
+      fetch.mockResponse('', { status: 404 });
 
-    loginScript.el.username.value = 'mrNobody';
-    loginScript.el.password.value = 'password';
-    expect(await mockLoginUser()).toBe(false);
-    expect(window.location.href).toEqual('login.html');
+      loginScript.el.username.value = 'mrNobody';
+      loginScript.el.password.value = 'password';
+      expect(await mockLoginUser()).toBe(false);
+      expect(window.location.href).toEqual('login.html');
+      expect(loginScript.el.loginPopup.textContent).toEqual('Not Found');
+    } catch(err) {
+      console.log(err);
+    }
+
+
+
   });
 
   test("Test invalid logging in of a user (incorrect password)", async () => {
-    fetch.mockResponse('', { status: 403 });
+    try {
+      fetch.mockResponse('', { status: 403 });
 
-    loginScript.el.username.value = 'username';
-    loginScript.el.password.value = 'incorrectPassword';
-    expect(await mockLoginUser()).toBe(false);
-    expect(window.location.href).toEqual('login.html');
+      loginScript.el.username.value = 'username';
+      loginScript.el.password.value = 'incorrectPassword';
+      expect(await mockLoginUser()).toBe(false);
+      expect(window.location.href).toEqual('login.html');
+      expect(loginScript.el.loginPopup.textContent).toEqual('Incorrect Password');
+    } catch(err) {
+      console.log(err);
+    }
+
+  });
+
+  test("Test invalid logging in of a user (bad request body)", async () => {
+    try {
+      fetch.mockResponse('', { status: 400 });
+
+      loginScript.el.username.value = undefined;
+      loginScript.el.password.value = 'incorrectPassword';
+      expect(await mockLoginUser()).toBe(false);
+      expect(window.location.href).toEqual('login.html');
+      expect(loginScript.el.loginPopup.textContent).toEqual('Bad Request');
+    } catch(err) {
+      console.log(err);
+    }
+
   });
 });
