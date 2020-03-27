b107e57bbe67149a0b91dc721b5d964d4e44b6e4
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2020-03-27 12:40:58 +0000

    add test for register

--- _tests_/register.test.js
+++ _tests_/register.test.js
@@ -9,7 +9,7 @@ function resetWindowLocation() {
   window.location.href = 'register.html';
 }
 
-describe("Test front-end/index.js", () => {
+describe("Test front-end/register.js", () => {
 
   beforeEach(() => {
     fetch.resetMocks();
@@ -21,21 +21,44 @@ describe("Test front-end/index.js", () => {
   const mockRegister = jest.fn(register.register);
 
   test("Test valid registering of a user", async () => {
-    fetch.mockResponse('', { status: 201 });
+    try {
+      fetch.mockResponse('', { status: 201 });
 
-    register.el.username.value = 'username';
-    register.el.password.value = 'password';
-    await mockRegister();
-    expect(window.location.href).toEqual('login.html');
+      register.el.username.value = 'username';
+      register.el.password.value = 'password';
+      await mockRegister();
+      expect(window.location.href).toEqual('login.html');
+    } catch(err) {
+      console.log(err);
+    }
   });
 
   test("Test duplicate registering of a user", async () => {
-    fetch.mockResponse('', { status: 409 });
+    try {
+      fetch.mockResponse('', { status: 409 });
 
-    register.el.username.value = 'username';
-    register.el.password.value = 'password';
-    expect(await mockRegister()).toBe(false);
-    expect(window.location.href).toEqual('register.html');
+      register.el.username.value = 'username';
+      register.el.password.value = 'password';
+      expect(await mockRegister()).toBe(false);
+      expect(window.location.href).toEqual('register.html');
+      expect(register.el.registerPopup.textContent).toBe('Conflict');
+    } catch(err) {
+      console.log(err);
+    }
+  });
+
+  test("Test registering of a user with bad request body", async () => {
+    try {
+      fetch.mockResponse('', { status: 400 });
+
+      register.el.username.value = ' ';
+      register.el.password.value = ' ';
+      expect(await mockRegister()).toBe(false);
+      expect(window.location.href).toEqual('register.html');
+      expect(register.el.registerPopup.textContent).toBe('Bad Request');
+    } catch(err) {
+      console.log(err);
+    }
   });
 
 });
