1a19ced07bf1063a62ed6d87c90205da8f8c1183
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2020-03-27 13:20:09 +0000

    configure jest setup to handle async promise rejections better

--- _tests_/setupJest.js
+++ _tests_/setupJest.js
@@ -4,6 +4,11 @@ const clientSideAPI = require('../front-end/javascript/client-side.js');
 global.search = clientSideAPI.search;
 global.registerUser = clientSideAPI.registerUser;
 global.login = clientSideAPI.login;
+global.getUserSchedule = clientSideAPI.getUserSchedule;
 
 delete window.location;
 global.window.location = {href: null};
+
+process.on('unhandledRejection', (reason) => {
+	console.log('REJECTION', reason);
+});
