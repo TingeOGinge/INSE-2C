e0fa1018a6a3c4448ef78e663ddd0efcd0ec1efa
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2021-01-31 18:11:06 +0000

    containerize the server and database microservices

--- server/server.js
+++ server/server.js
@@ -2,4 +2,4 @@ const app = require('./ms_entry');
 
 
 // Start server on localhost:5000
-app.listen(5000, () => console.log('Server started on port 5000'));
+app.listen(5000, "0.0.0.0", () => console.log('Server started on port 5000'));
---
+++ server/wait-for-postgres.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DB_HOST="192.168.0.11"
+DB_PORT="5432"
+
+while ! pg_isready -h ${DB_HOST} -p ${DB_PORT} > /dev/null 2> /dev/null; do
+    echo "Connecting to ${DB_HOST} Failed"
+    sleep 1
+done
+
+node db-data/populateRecipes.js
+npm run start
