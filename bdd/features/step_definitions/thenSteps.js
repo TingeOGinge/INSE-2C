de0eff8679fc39f286d0f6a0c83ee8e60dc71eaf
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2021-02-03 22:58:46 +0000

    include numerics when checking res values

--- bdd/features/step_definitions/thenSteps.js
+++ bdd/features/step_definitions/thenSteps.js
@@ -2,6 +2,10 @@ const { Then } = require('@cucumber/cucumber');
 const assert = require('assert');
 
 Then ('the response body contains {string} {string}', async function (key, value) {
-    const responseBody = await this.response.json();
-    assert(responseBody[key] = value);
+    this.response.responseValue = this.response.jsonResponseBody[key];
+    if (!isNaN(this.response.responseValue) && !isNaN(value)) {
+        this.response.responseValue = parseFloat(this.response.responseValue);
+        value = parseFloat(value);
+    }
+    assert(this.response.responseValue === value);
 });
--- bdd/features/test.feature
+++ bdd/features/test.feature
@@ -3,8 +3,10 @@ Feature: Container test
   Scenario Outline: check recipe by id 
     Given a live server
     When I call the api route "<route>"
-    Then the response body contains "<key>" "<value>"
+    And I resolve the json response body
+    Then the response body contains "<key1>" "<value1>"
+    And the response body contains "<key2>" "<value2>"
 
   Examples:
-  | route | key | value |
-  | /api/getRecipe/10 | recipe_name | Chorizo carbonara |
+  | route | key1 | value1 | key2 | value2 |
+  | /api/getRecipe/10 | recipe_name | Chorizo carbonara | cooking_minutes | 15 |
