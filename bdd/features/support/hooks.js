c41300e7115a598eabb781da0a51e32ad6a60977
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2021-02-05 11:28:38 +0000

    add testcontainers in to Before/AfterAll steps

--- bdd/features/support/hooks.js
+++ bdd/features/support/hooks.js
@@ -1,5 +1,28 @@
 const {AfterAll, BeforeAll, After} = require('@cucumber/cucumber');
+const { DockerComposeEnvironment, Wait } = require("testcontainers");
+const path = require('path');
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 After(async function () {
-    this.response = null;
+  this.response = undefined;
 });
+
+BeforeAll({ timeout: 60 * 1000 }, async function () {
+  const composeFilePath = path.resolve(__dirname, "..", "..", "..");
+  const composeFile = 'docker-compose.yml'
+
+  this.environment = await new DockerComposeEnvironment(composeFilePath, composeFile)
+      .withBuild()
+      .withWaitStrategy("inse2c_server", Wait.forLogMessage("Server started on port 5000"))
+      .up();
+      
+  this.server = this.environment.getContainer("inse2c_server");
+});
+    
+AfterAll({ timeout: 60 * 1000 }, async function () {
+  await this.environment.down();
+});
+
