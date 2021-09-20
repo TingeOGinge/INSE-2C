843429cfd6abf2fbb4e526e74ff042bca75c32bf
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2021-09-20 17:44:07 +0100

    call database search through algorithm MS

--- server/ms_entry.go
+++ server/ms_entry.go
@@ -6,19 +6,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 
 	"github.com/TingeOGinge/INSE-2C/server/database"
+	"github.com/TingeOGinge/INSE-2C/server/algorithm"
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 type Env struct {
-	ecochef interface {
-		MainSearch(parameters []string) ([]database.Recipe, error)
+	algorithm interface {
+		Search(parameters url.Values) ([]database.Recipe, error)
 	}
 }
 
 func (env *Env) searchHandler (w http.ResponseWriter, r *http.Request) {
-	recipes, err := env.ecochef.MainSearch(r.URL.Query()["searchTerms"])
+	recipes, err := env.algorithm.Search(r.URL.Query())
 	if err != nil {
 		errString := fmt.Sprintf("Error executing query: %v\n", err)
 		http.Error(w, errString, http.StatusInternalServerError)
@@ -57,7 +59,9 @@ func main() {
 	}
 
 	env := &Env{
-		ecochef: database.EcochefEnv{DBpool: dbpool},
+		algorithm: algorithm.AlgEnv{
+			DB: database.DbEnv{DBpool: dbpool},
+		},
 	}
 
 	http.HandleFunc("/api/mainSearch", makeHandler(env.searchHandler))
