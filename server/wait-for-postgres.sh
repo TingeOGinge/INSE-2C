a07c7eabccc62ab0542d9df65300e0e387717264
Dillon O'Shea <dillon.oshea@myport.ac.uk>
2021-09-22 09:24:13 +0100

    use default DC network bridge properly

--- server/wait-for-postgres.sh
+++ server/wait-for-postgres.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while ! pg_isready -h ${DB_HOST} -p ${DB_PORT} > /dev/null 2> /dev/null; do
+while ! pg_isready -h db -p 5432 > /dev/null 2> /dev/null; do
     echo "Connecting to ${DB_HOST} Failed"
     sleep 1
 done
